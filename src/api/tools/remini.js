af60144ea2ee067fb507fc2ce6444274c26ae122
ShotaQuen <imanadja86@gmail.com>
2025-05-19 19:12:09 +0800

    update

---
+++ src/api/tools/remini.js
@@ -0,0 +1,68 @@
+const axios = require('axios')
+const cheerio = require('cheerio') 
+
+async function remini(urlPath, method) {
+	return new Promise(async (resolve, reject) => {
+		let Methods = ["enhance", "recolor", "dehaze"];
+		Methods.includes(method) ? (method = method) : (method = Methods[0]);
+		let buffer,
+			Form = new FormData(),
+			scheme = "https" + "://" + "inferenceengine" + ".vyro" + ".ai/" + method;
+		Form.append("model_version", 1, {
+			"Content-Transfer-Encoding": "binary",
+			contentType: "multipart/form-data; charset=utf-8",
+		});
+		Form.append("image", Buffer.from(urlPath), {
+			filename: "enhance_image_body.jpg",
+			contentType: "image/jpeg",
+		});
+		Form.submit(
+			{
+				url: scheme,
+				host: "inferenceengine" + ".vyro" + ".ai",
+				path: "/" + method,
+				protocol: "https:",
+				headers: {
+					"User-Agent": "okhttp/4.9.3",
+					Connection: "Keep-Alive",
+					"Accept-Encoding": "gzip",
+				},
+			},
+			function (err, res) {
+				if (err) reject();
+				let data = [];
+				res
+					.on("data", function (chunk, resp) {
+						data.push(chunk);
+					})
+					.on("end", () => {
+						resolve(Buffer.concat(data));
+					});
+				res.on("error", (e) => {
+					reject();
+				});
+			}
+		);
+	});
+}
+
+module.exports = function (app) {
+  app.get('/tools/remini', async (req, res) => {
+  	const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ status: false, error: "Query parameter 'url' is required" });
+  }
+
+  try {
+    const anuge = await remini(url);
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': anuge.length,
+            });
+            res.end(anuge);
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message });
+  }
+});
+}
