f07b1d3b9c773b5242b5d00f3430727f30cdbffe
ShotaQuen <imanadja86@gmail.com>
2025-05-19 21:14:57 +0800

    update

---
+++ src/api/search/xnxx.js
@@ -0,0 +1,23 @@
+module.exports = function (app) {
+  app.get('/search/xnxx', async (req, res) => {
+
+  const { q } = req.query;
+
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Query parameter 'q' is required" });
+  }
+  
+  const xnxx = await axios.get(`https://api.vreden.my.id/api/xnxxsearch?query=${q}`);
+  
+  try {
+    const response = await xnxx(q);
+    res.status(200).json({
+      status: true,
+      creator: 'ikann',
+      data: response.data.result
+    });
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message });
+  }
+});
+}
