e0cdbc9a2c09c0537adf6c2db076b837f99c764d
ShotaQuen <imanadja86@gmail.com>
2025-05-22 09:21:24 +0800

    update status.js

--- src/api/owner/status.js
+++ src/api/owner/status.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const validateApiKey = require('./middleware/apikey');
+const validateApiKey = require('./middleware');
 
-router.get('/api/test', validateApiKey, (req, res) => {
+ module.exports = function (app) {
+app.get('/api/test', validateApiKey, (req, res) => {
   res.json({ success: true, message: `Hello, ${req.apikeyOwner}!` });
 });
-
-module.exports = router;
+}
