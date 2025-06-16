6d9c0b0de56ebed02d6a4cf4418c4471e0b1910d
ShotaQuen <imanadja86@gmail.com>
2025-06-16 15:55:26 +0800

    update

---
+++ src/api/tools/ustad.js
@@ -0,0 +1,25 @@
+module.exports = function (app) {
+    const { text } = req.query;
+    async function bluearchive() {
+        try {
+            const { response } = await axios.get(`https://api.taka.my.id/tanya-ustad?quest=${text}`)
+            const anuxx = response.data
+            return Buffer.from(anuxx);
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+app.get('/tools/ustadz', async (req, res) => {
+    try {
+            const pedo = await bluearchive();
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': pedo.length,
+            });
+            res.end(pedo);
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+  })
+}
