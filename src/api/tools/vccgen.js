a661eebe8c26e918548923bda2ce093c0c23e9bc
ShotaQuen <imanadja86@gmail.com>
2025-06-17 20:05:40 +0800

    update

---
+++ src/api/tools/vccgen.js
@@ -0,0 +1,41 @@
+const { vccgen } = require('@ikanngeming/blubub')
+const typecard = ["visa", "americanexpress", "mastercard", "jcb"];
+
+module.exports = function (app) {
+  app.get('/tools/vccgen', async (req, res) => {
+    const { type, count } = req.query;
+    
+    if (!typecard.includes(type)) {
+      return res.status(400).json({
+          status: false,
+          error: "Masukkan type nya, visa, americanexpress, mastercard, jcb"
+        })
+      }
+    
+      if (!count && count > 10) {
+        return res.status(400).json({
+          status: false,
+          error: "Masukkan jumlah vcc nya, dan jangan sampai melebihi 10 generate vcc"
+        })
+      }
+      
+      let typeds;
+    switch (type) {
+      case 'visa': typeds = "Visa"; break;
+      case 'mastercard': typeds = "Mastercard"; break;
+      case 'americanexpress': typeds = "American%20Express"; break;
+      case 'jcb': typeds = "JCB"; break;
+      default: return res.status(400).json({ status: false, message: "Invalid card type" });
+    }
+      try {
+        let respon = vccgen(type, count)
+        res.status(200).json({
+          status: true,
+          creator: 'ikann',
+          result: respon
+        })
+      } catch (error) {
+    res.status(500).json({ status: false, error: error.message });
+  }
+  })
+}
