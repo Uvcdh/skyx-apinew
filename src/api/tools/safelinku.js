986ca4e2eb7dc183b564bebacddac56417563690
ShotaQuen <imanadja86@gmail.com>
2025-06-20 23:39:57 +0800

    update

--- src/api/tools/safelinku.js
+++ src/api/tools/safelinku.js
@@ -1,27 +1,7 @@
-async function sfl(longUrl) {
-  const token = "93ffffc5750be818467b75683f1d12114eed5f5d";
-  const apiUrl = "https://safelinku.com/api/v1/links";
-  const alias = generateAlias(); // ✅ alias alfanumerik
-
-  try {
-    const response = await axios.post(apiUrl, {
-      url: longUrl,
-      alias: alias
-    }, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      }
-    });
-
-    return response.data;
-  } catch (err) {
-    return err.response?.data || err.message;
-  }
-}
+const { sfl } = require('@ikanngeming/blubub');
 
 module.exports = function(app) {
-    app.get('/tools/sfl', async (req, res) => {
+    app.get('/tools/slf', async (req, res) => {
         try {
           const { link } = req.query;
             const sl = await sfl(link);
